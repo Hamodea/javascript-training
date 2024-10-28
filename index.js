af2762653251001abaeba1c0f05fadcfb9394668
Hamodea <mohmadwahbi22@gmail.com>
2024-10-28 21:39:07 +0100

    uppdate

--- index.js
+++ index.js
@@ -15,3 +15,26 @@ function askUser (){
       }
     
   }
+
+
+  // Fråga användaren om tre olika ord
+function words(){
+    let word1 = prompt("Skriv in det första ordet:");
+    let word2 = prompt("Skriv in det andra ordet:");
+    let word3 = prompt("Skriv in det tredje ordet:");
+
+    // Kontrollera vilket ord som är längst
+    let longestWord = word1;
+
+    if (word2.length > longestWord.length) {
+        longestWord = word2;
+    }
+
+    if (word3.length > longestWord.length) {
+        longestWord = word3;
+    }
+
+// Skriv ut det längsta ordet
+    alert("Det längsta ordet är: " + longestWord);
+
+}
